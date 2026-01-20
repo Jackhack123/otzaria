11af3031d2f47f34f4b486094c13b6c48ad133c7
Jackhack123 <91012838+Jackhack123@users.noreply.github.com>
2026-01-20 13:45:11 +0000

    fix

--- lib/widgets/ad_popup_dialog.dart
+++ lib/widgets/ad_popup_dialog.dart
@@ -380,7 +380,7 @@ class _AdPopupDialogState extends State<AdPopupDialog>
                 side: BorderSide(color: Colors.grey.shade400),
               ),
               icon: const Icon(Icons.close, size: 18),
-              label: const Text('Do Not Show Again'),,
+              label: const Text('Do Not Show Again'),
             ),
           ),
         ),
